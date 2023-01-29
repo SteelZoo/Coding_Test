e75dee07349a63e40f1f7fa54e757d00e368cfa1
SteelZoo <cjfwn96@gmail.com>
2023-01-29 23:00:19 +0900

    프로그래머스/연습문제/평균 구하기

---
+++ 프로그래머스/평균 구하기/Average.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(arr: IntArray): Double {
+        var answer = 0
+        arr.forEach{
+            answer += it
+        }
+        return answer.toDouble()/arr.size
+    }
+}
